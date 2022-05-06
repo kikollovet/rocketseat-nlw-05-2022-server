b14d60183bb892daa235e85676458df051a81bf8
Kiko <kikollovet@gmail.com>
2022-05-06 14:42:00 -0300

    porta listen mais uma vezzz

--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,11 @@ import cors from 'cors';
 import { routes } from './routes';
 
 const app = express();
-const PORT = process.env.PORT//|| 3333
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(PORT, ()=>{
+app.listen(process.env.$PORT || 3333, ()=>{
     console.log('HTTP server runing!');
 });
